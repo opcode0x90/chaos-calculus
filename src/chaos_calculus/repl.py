02cd3b514f89b4af78e945067133b09fea2e4a2d
opcode0x90 <opcode0x90.nop@gmail.com>
2022-10-21 00:55:36 +0800

    Refactored repl into dedicated module

---
+++ src/chaos_calculus/repl.py
@@ -0,0 +1,55 @@
+from typing import Optional
+
+import click
+
+try:
+    # macOS
+    import gnureadline as readline
+except ImportError:
+    # everyone else
+    import readline
+
+################################################################################
+
+
+class Repl:
+    """Basic implementation of read-eval-print-loop interface."""
+
+    def __init__(self, prompt: str, prefill: Optional[str] = None, banner: Optional[str] = None):
+        self.prompt = prompt
+        self.prefill = prefill
+        self.banner = banner
+
+    def __enter__(self):
+        if self.banner:
+            # display banner
+            click.echo(self.banner)
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        # ignore
+        pass
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> str:
+
+        def _hook():
+            readline.insert_text(self.prefill)
+            readline.redisplay()
+
+        if self.prefill:
+            # use readline to prefill the answers
+            readline.set_pre_input_hook(_hook)
+
+        # prompt for input
+        value = click.prompt(self.prompt, type=str)
+
+        # clear the input hook for next prompt
+        readline.set_pre_input_hook()
+
+        # reuse the value for next prefill
+        self.prefill = value
+
+        return value
--- src/chaos_calculus/scripts/cli.py
+++ src/chaos_calculus/scripts/cli.py
@@ -1,3 +1,4 @@
+import textwrap
 import time
 from contextlib import contextmanager
 
@@ -5,6 +6,7 @@ import click
 import keras_cv
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from chaos_calculus.repl import Repl
 from tensorflow import keras
 
 try:
@@ -26,45 +28,53 @@ IMAGES = 9
 
 @click.command()
 def main():
-    click.echo("Initializing Stable Diffusion...")
-    with timing("Model initialized"):
-        # model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
-        model = keras_cv.models.StableDiffusion(img_width=512, img_height=512, jit_compile=True)
-        # model = keras_cv.models.StableDiffusion(img_width=640, img_height=640, jit_compile=True)
-        # model = keras_cv.models.StableDiffusion(img_width=640, img_height=1080 - 32, jit_compile=True)
-    click.echo()
+    banner = textwrap.dedent("""\
+           ________                        ______      __           __
+          / ____/ /_  ____ _____  _____   / ________ _/ _______  __/ __  _______
+         / /   / __ \/ __ `/ __ \/ ___/  / /   / __ `/ / ___/ / / / / / / / ___/
+        / /___/ / / / /_/ / /_/ (__  )  / /___/ /_/ / / /__/ /_/ / / /_/ (__  )
+        \____/_/ /_/\__,_/\____/____/   \____/\__,_/_/\___/\__,_/_/\__,_/____/
+
+        (Press Ctrl+C multiple times to abort.)
+        """)
 
     # prompt = "photograph of an astronaut riding a horse"
     prompt = "ultra-detailed. uhd 8k, artstation, cryengine, octane render, unreal engine. photograph of an astronaut riding a horse"
 
-    while True:
-        if not (prompt := get_prompt(prompt)):
-            continue
+    with Repl("Prompt", prefill=prompt, banner=banner) as repl:
+        click.echo("Initializing Stable Diffusion...")
+        with timing("Model initialized"):
+            # model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+            model = keras_cv.models.StableDiffusion(img_width=512, img_height=512, jit_compile=True)
+            # model = keras_cv.models.StableDiffusion(img_width=640, img_height=640, jit_compile=True)
+            # model = keras_cv.models.StableDiffusion(img_width=640, img_height=1080 - 32, jit_compile=True)
+        click.echo()
 
-        positive, _, negative = prompt.partition("~")
-        positive = positive.strip()
-        negative = negative.strip()
+        for prompt in repl:
+            positive, _, negative = prompt.partition("~")
+            positive = positive.strip()
+            negative = negative.strip()
 
-        with timing("Image generated"):
-            encoded_positive = model.encode_text(positive)
-            model._get_unconditional_context = lambda: model.encode_text(negative)
+            with timing("Image generated"):
+                encoded_positive = model.encode_text(positive)
+                model._get_unconditional_context = lambda: model.encode_text(negative)
 
-            images = model.generate_image(encoded_positive, batch_size=IMAGES)
-            # images = []
-            # for _ in range(3):
-            #     im = model.generate_image(encoded_positive)
-            #     images.extend(im)
+                images = model.generate_image(encoded_positive, batch_size=IMAGES)
+                # images = []
+                # for _ in range(3):
+                #     im = model.generate_image(encoded_positive)
+                #     images.extend(im)
 
-        plt.figure(figsize=(19.2, 10.8))
-        for i in range(len(images)):
-            plt.subplots_adjust(0, 0, 1, 1, 0, 0)
-            plt.subplot(len(images) // MAX_COLUMN, MAX_COLUMN, i + 1)
-            plt.imshow(images[i])
-            plt.axis("off")
+            plt.figure(figsize=(19.2, 10.8))
+            for i in range(len(images)):
+                plt.subplots_adjust(0, 0, 1, 1, 0, 0)
+                plt.subplot(len(images) // MAX_COLUMN, MAX_COLUMN, i + 1)
+                plt.imshow(images[i])
+                plt.axis("off")
 
-        mgr = plt.get_current_fig_manager()
-        mgr.window.state('zoomed')
-        plt.show()
+            mgr = plt.get_current_fig_manager()
+            mgr.window.state('zoomed')
+            plt.show()
 
 
 ################################################################################
@@ -75,16 +85,3 @@ def timing(prompt: str):
     start = time.monotonic()
     yield
     click.echo(f"{prompt} in {time.monotonic() - start:.2f} seconds.")
-
-
-def get_prompt(prefill) -> str:
-
-    def hook():
-        readline.insert_text(prefill)
-        readline.redisplay()
-
-    readline.set_pre_input_hook(hook)
-    prompt = input("Prompt (Ctrl+C and Enter to quit): ")
-    readline.set_pre_input_hook()
-
-    return prompt
